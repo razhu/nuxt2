c3060124b493742ad797ff763499f0762b4e871b
Ramiro <rhuayller@agetic.gob.bo>
2017-08-09 15:45:52 -0400

    ok

---
+++ pages/Test.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+  <h1>test page</h1>  
+  <input type="" name="" value="" v-model="myTitle">
+  </div>
+  
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      myTitle: 'rock'
+    }
+  },
+  head: {
+    title: this.myTitle
+  }
+}
+</script>
+
+<style>
+  
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <section class="hero is-medium is-primary is-bold">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">
+            Welcome to the JavaScript SSR Blog.
+          </h1>
+          <h2 class="subtitle">
+            Hope you find something you like.
+          </h2>
+        </div>
+      </div>
+    </section>
+    <post-list></post-list>
+  </div>
+</template>
+
+<script>
+  import PostList from '~/components/Posts.vue'
+  export default {
+    head: {
+      title: 'Home'
+    },
+    components: {
+      PostList
+    }
+  }
+</script>
